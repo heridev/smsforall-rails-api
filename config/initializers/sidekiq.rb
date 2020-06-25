83796c409858b0ddc3e9074dcd6fb2a260aada90
heriberto perez magaña <joseheribertoperezmagana@gmail.com>
2020-06-25 16:21:07 -0500

    Setup Sidekiq as a worker

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,32 @@
+if Rails.env.production? || Rails.env.staging?
+
+  Sidekiq.configure_client do |config|
+    # As a rule of thumb, the Sidekiq client, which is usually a
+    # rails app will only need 1 connection to redis. I used 2
+    # in the config above to handle rare cases where one of
+    # the connection hangs and at least the client has a backup to use.
+    redis_client_size = Integer(ENV['REDIS_CLIENT_SIZE'] || 2)
+    config.redis = { url: ENV['REDISTOGO_URL'], size: redis_client_size }
+  end
+
+  Sidekiq.configure_server do |config|
+    # in case you do not what value to use just look into the
+    # plan you're using https://cl.ly/1p1v0O1q2V2X
+    # 10 for free tier and 50 connections for micro as an example
+    # minium of 30 or you will get the error
+    # Your Redis connection pool is too small for Sidekiq to work,
+    # your pool has 10 connections but really needs to have at least 27
+    redis_server_connection_number = Integer(ENV['REDIS_SERVER_SIZE'] || 30)
+    config.redis = { url: ENV['REDISTOGO_URL'], size: redis_server_connection_number }
+
+    # Number of concurrent workers pulling jobs and processing them
+    # every one will use a database connection(postgresql) and a redis
+    # connection(calculate based on your current plan)
+    # By default Sidekiq uses 25 threads if you do not specify this value
+    # NOTE: when increasing this value take into account the database connection and
+    # puma threads in the config/puma.rb file
+    sidekiq_concurrency = Integer(ENV['SIDEKIQ_CONCURRENCY_WORKERS'] || 5)
+    config.options[:concurrency] = sidekiq_concurrency
+  end
+end
+
--- config/puma.rb
+++ config/puma.rb
@@ -1,38 +1,41 @@
-# Puma can serve each request in a thread from an internal thread pool.
-# The `threads` method setting takes two numbers: a minimum and maximum.
-# Any libraries that use thread pools should be configured to match
-# the maximum value specified for Puma. Default is set to 5 threads for minimum
-# and maximum; this matches the default thread size of Active Record.
-#
-max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
-min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
-threads min_threads_count, max_threads_count
+# take into account database connections total when increasing this
+# values for that look at the database.yml into the pool section
+min_threads = Integer(ENV['PUMA_MIN_THREADS'] || 0)
+max_threads = Integer(ENV['PUMA_MAX_THREADS'] || 3)
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-#
+# 1 thread means 1 request served
+# threads minimum_threads_available, maximum_threads_available per worker
+threads min_threads, max_threads
+
 port        ENV.fetch("PORT") { 3000 }
-
-# Specifies the `environment` that Puma will run in.
-#
 environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Specifies the `pidfile` that Puma will use.
-pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
-
-# Specifies the number of `workers` to boot in clustered mode.
-# Workers are forked web server processes. If using threads and workers together
-# the concurrency of the application would be max `threads` * `workers`.
-# Workers do not work on JRuby or Windows (both of which do not support
-# processes).
-#
-# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
-
-# Use the `preload_app!` method when specifying a `workers` number.
-# This directive tells Puma to first boot the application and load code
-# before forking the application. This takes advantage of Copy On Write
-# process behavior so workers use less memory.
-#
-# preload_app!
+pidfile ENV.fetch('PIDFILE') { 'tmp/pids/server.pid' }
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+
+# puma workers are copies of your application and that
+# means every copy will handle its own threads
+workers Integer(ENV['WEB_CONCURRENCY'] || 1)
+
+preload_app!
+
+before_fork do
+  ActiveRecord::Base.connection.disconnect!
+
+  if Rails.env.staging? || Rails.env.development?
+    @sidekiq_pid ||= spawn('bundle exec sidekiq -c 2 -q default -q mailers')
+  end
+end
+
+on_worker_boot do
+  # Worker specific setup for Rails 4.1+
+  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+  ActiveRecord::Base.establish_connection
+end
+
+on_restart do
+  Sidekiq.redis.shutdown { |conn| conn.close }
+end
