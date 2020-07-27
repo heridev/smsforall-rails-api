5153ec4fc94a445a0bc5f21c185dc48d4ce2daf1
heriberto perez magaña <joseheribertoperezmagana@gmail.com>
2020-07-26 21:40:20 -0500

    Fixes daily and minute control when using env variables

--- http_requester_service.rb
+++ http_requester_service.rb
@@ -25,9 +25,17 @@ class HttpRequesterService
   end
 end
 
+# locahost
+# headers = {
+#   'Authorization-Token': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxM30.1KX29F-3h6gJYsnntAQH08NWlEAD91utxoMh8J_sfaw',
+#   'Authorization-Client': '$2a$12$ZL6.AGqVM7QWZa80Olm/m.',
+#   'Content-Type': 'application/json'
+# }
+
+# production
 headers = {
-  'Authorization-Token': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxM30.1KX29F-3h6gJYsnntAQH08NWlEAD91utxoMh8J_sfaw',
-  'Authorization-Client': '$2a$12$ZL6.AGqVM7QWZa80Olm/m.',
+  'Authorization-Token': 'eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjozfQ.S7-mMxhL76vwmDEHiBoC25b6OKWCGFfROZvUeaUHFoo',
+  'Authorization-Client': '$2a$12$Spip/ZntEXFlgcjlB3Y4Ge',
   'Content-Type': 'application/json'
 }
 
@@ -35,19 +43,28 @@ tablecast_number = '+523121698456'
 xiaomi_rosa_number = '+523121231639'
 personal_number = '+523121231517'
 
-50.times do |number|
+200.times do |number|
+  sleep 0.5
   values = %i(juan pepe chuy heriberto andrea pako fabian roberto irvin chuy mary maria juanit alberto roberto luis eduardo eddie kevin cone alex rafa)
   body = {
-    'sms_number': tablecast_number,
+    # 'sms_number': tablecast_number,
+    # 'sms_number': xiaomi_rosa_number,
+    'sms_number': personal_number,
     # invalid number
     # 'sms_number': '+5231212315',
     'sms_content': "hola #{values.sample} andas por alli? - #{number} - #{Time.now.to_i}",
     'sms_customer_reference_id': "#{number} - #{Time.now.to_i}",
-    'mobile_hub_id': '31b819ef-37bd-4ecc-bae4-bc2dd52dfb58',
+    # locahost
+    # 'mobile_hub_id': '31b819ef-37bd-4ecc-bae4-bc2dd52dfb58',
+    # api.smsparatodos.com
+    'mobile_hub_id': '49b04bad-30f7-4dd5-a9b1-ab90939e0f93',
     'sms_type': 'standard_delivery'
   }
 
-  response = HttpRequesterService.make('https://smsparatodosapi.ngrok.io/v2/sms/create', 'Post', headers, body)
+  # localhost
+  # response = HttpRequesterService.make('https://smsparatodosapi.ngrok.io/v2/sms/create', 'Post', headers, body)
+  # production
+  response = HttpRequesterService.make('https://api.smsparatodos.com/v2/sms/create', 'Post', headers, body)
   puts response.code
   puts response.read_body
 end
