54f1b5b9560bcb6bb678c8cb7d3df1cc5441990c
heriberto perez magaña <joseheribertoperezmagana@gmail.com>
2020-08-10 23:54:37 -0500

    Adds some logs to see what's going on

--- app/jobs/sms_hub_interval_sender_notification_job.rb
+++ app/jobs/sms_hub_interval_sender_notification_job.rb
@@ -4,7 +4,9 @@ class SmsHubIntervalSenderNotificationJob < ApplicationJob
   queue_as :standard_delivery
 
   def perform
+    puts 'outside active do ============================'
     SmsMobileHub.active.pluck(:id).each do |hub_id|
+    puts "==========#{hub_id}============================"
       SmsHubNotificationSenderJob.perform_later(
         hub_id
       )
