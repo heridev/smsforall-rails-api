abc932e51abb3783bbb2e66e7d23dfb6a351164a
heriberto perez magaña <joseheribertoperezmagana@gmail.com>
2020-08-11 20:33:52 -0500

    Implements new sms_notifications#receive endpoint for android
    - Improves sms hub notification check to be sent only in office hours
    - Fixes a few Rspec tests
    - Removes a few puts messages for debugging purposes

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -29,6 +29,8 @@ RSpec.configure do |config|
 
   config.include CommonHelpers
 
+  config.include ActiveSupport::Testing::TimeHelpers
+
   config.use_transactional_fixtures = true
 
   config.infer_spec_type_from_file_location!
