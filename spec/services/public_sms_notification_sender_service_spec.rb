f006697f7987391f89b79063bfd8ca8d3ac42efb
heriberto perez magaña <joseheribertoperezmagana@gmail.com>
2021-07-28 20:52:25 -0500

    Adds new endpoint on V1 to accept public submission of sms messages

---
+++ spec/services/public_sms_notification_sender_service_spec.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe PublicSmsNotificationSenderService do
+  let(:user) { create(:user, mobile_number: '3121899980') }
+  let!(:sms_mobile_hub_master) do
+    user = create(
+      :user,
+      mobile_number: '3121231617',
+      country_international_code: '52'
+    )
+    create(
+      :sms_mobile_hub,
+      is_master: true,
+      user: user,
+      device_name: 'hub master'
+    )
+  end
+
+  describe '#send_notification' do
+    context 'when the sms params are valid' do
+      let(:sms_content) { 'Que onda compi, como te va?' }
+      let(:sms_number) { '+523121231517' }
+
+      it 'responds with the sms notification details' do
+        argument_params = {
+          sms_number: sms_number,
+          sms_content: sms_content
+        }
+
+        result = described_class.send_notification(argument_params)
+        expect(result[:status]).to eq 'enqueued'
+        expect(result[:api_version]).to eq 'V2'
+        expect(result[:mobile_hub_id]).to eq sms_mobile_hub_master.reload.uuid
+        expect(result[:sms_content]).to eq sms_content
+        expect(sms_number).to match(result[:sms_number])
+      end
+
+      it 'enqueues an urgent sms notification to be sent' do
+        argument_params = {
+          sms_number: sms_number,
+          sms_content: sms_content
+        }
+
+        described_class.send_notification(argument_params)
+        job = find_enqueued_job_by(UrgentSmsNotificationSenderJob)
+        expect(job[:args].size).to eq 2
+        expect(job[:job]).to eq UrgentSmsNotificationSenderJob
+        expect(job[:queue]).to eq 'urgent_delivery'
+      end
+    end
+  end
+end
+
