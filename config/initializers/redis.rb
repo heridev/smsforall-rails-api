f98de901f236bc5f33a61e8c5f138735c262ee4a
heriberto perez magaña <joseheribertoperezmagana@gmail.com>
2023-03-21 19:05:30 -0600

    Disables usage of SSL on redis connection as Heroku handles that

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -3,7 +3,10 @@
 module ReadCache
   class << self
     def redis
-      @redis ||= Redis.new(url: ENV.fetch('REDIS_URL'))
+      @redis ||= Redis.new(
+        url: ENV.fetch('REDIS_URL'),
+        ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+      )
     end
   end
 end
