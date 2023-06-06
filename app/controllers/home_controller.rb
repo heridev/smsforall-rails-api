23652a7a7705e79b6fb48589738e08628027cf35
heriberto perez magaña <joseheribertoperezmagana@gmail.com>
2023-06-05 19:03:10 -0600

    Updates domain name to smsforall.org

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ActionController::API
   def index
     api_version_hash = {
       current_version: 'v1',
-      domain_name: 'smsparatodos.com'
+      domain_name: 'smsforall.org'
     }
     render json: api_version_hash
   end
