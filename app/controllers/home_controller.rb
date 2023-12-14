db619becc1d4ad896df07a34c3f01a13a5e484ed
heriberto perez magaña <joseheribertoperezmagana@gmail.com>
2023-12-13 23:28:49 -0600

    Increases to v2 the API number as we just migrated into FCM V1

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ActionController::API
   def index
     api_version_hash = {
-      current_version: 'v1',
+      current_version: 'v2',
       domain_name: 'smsforall.org'
     }
     render json: api_version_hash
